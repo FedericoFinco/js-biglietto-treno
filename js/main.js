b5c4dd41cc94c09d9d09f42be4eb7a816fb32d3d
FedericoFinco <doppiaeffe94@gmail.com>
2023-04-14 18:59:51 +0200

    dev (main) : all function dove, not responsive tho

--- js/main.js
+++ js/main.js
@@ -7,23 +7,41 @@ const tariffa = 0.21;
 const scontoOver65 = 0.6;
 const scontoMinorenni = 0.8;
 
+const outputBiglietto = document.getElementById("output-biglietto")
+
+
 const bottone = document.getElementById("button");
+const bottoneBack = document.getElementById("button-back");
 bottone.addEventListener("click", function () {
   let prezzoBiglietto;
   let tipoBiglietto;
   if (eta.value === "minorenne") {
-    prezzoBiglietto = tariffa * parseInt(km.value) * scontoMinorenni;
+    prezzoBiglietto = tariffa * Number(km.value) * scontoMinorenni;
     tipoBiglietto = "Biglietto Minorenni";
   } else if (eta.value === "over65") {
-    prezzoBiglietto = tariffa * parseInt(km.value) * scontoOver65;
+    prezzoBiglietto = tariffa * Number(km.value) * scontoOver65;
     tipoBiglietto = "Biglietto over 65";
   } else {
-    prezzoBiglietto = tariffa * parseInt(km.value);
+    prezzoBiglietto = tariffa * Number(km.value);
     tipoBiglietto = "Biglietto Standard";
   }
+
+  document.getElementById("nome-passeggero").innerHTML=nome.value
+  document.getElementById("tipo-biglietto").innerHTML=tipoBiglietto
+  document.getElementById("prezzo-biglietto").innerHTML=prezzoBiglietto.toFixed(2)
+  document.getElementById("num-carrozza").innerHTML=Math.floor(Math.random() * 10)
+  document.getElementById("codice-cp").innerHTML=Math.floor(Math.random() * 100000)
   console.log("Nominativo: ", nome.value)
   console.log("Prezzo: ", prezzoBiglietto);
   console.log("Tipologia biglietto: ", tipoBiglietto);
   console.log("codice cp: ", Math.floor(Math.random() * 100000));
   console.log("N° carrozza: ", Math.floor(Math.random() * 10));
+  outputBiglietto.className = "container rounded bgSecondary p-2"
+});
+
+bottoneBack.addEventListener("click", function () {
+  outputBiglietto.className = "container rounded bgSecondary p-2 d-none"
+  nome.value=""
+  eta.value=""
+  km.value=""
 });
